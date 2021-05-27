3e3e02a1c63f7d89f689719b0d2964eebb9bc434
AbsolutelyNoFires <absolutelynofires@gmail.com>
2021-05-27 10:48:46 -0500

    Fixed finally, thanks to u/01010100

--- DesignerMode.cs
+++ DesignerMode.cs
@@ -6,7 +6,7 @@ namespace DesignerMode
     public class DesignerMode : AuroraPatch.Patch
     {
         public override string Description => "Hit the save button to enter Designer Mode";
-        protected override void Loaded(Harmony harmony)
+        protected override void Started()
         {
             // thanks to u/01010100 for help
             Control savebtn = TacticalMap.Controls.Find("cmdToolbarSave", true)[0];
